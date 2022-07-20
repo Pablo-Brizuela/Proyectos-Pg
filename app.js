a547b8ab71810c8e5678691d46092fe9642c1d0e
ventas1 <ventas1@RACECOTT.LOCAL>
2022-07-20 16:53:48 -0300

    Terminado primera parte del video

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { homedir } = require("os");
+
 const path = require("path")
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, "./public")
+const publicFoldersPath = path.resolve(__dirname, "./public")
 
-app.use(express.static(publicPath))
+app.use(express.static(publicFoldersPath))
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000")
